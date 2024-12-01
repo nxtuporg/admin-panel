020bb5359374a681925130286f170a914870a7dc
Aditya kumar <adityakr80510@gmail.com>
2024-12-02 01:58:36 +0530

    Initial commit

---
+++ src/app/Signup/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "../../../components/button";
+
+const page = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+};
+
+export default page;
