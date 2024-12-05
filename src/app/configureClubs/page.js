08832782e76f7ce1897942917f2be415c80b6547
Udit Jain <uditjainstjis@gmail.com>
2024-12-05 15:58:38 +0530

    udits clubs page

--- src/app/configureClubs/page.js
+++ src/app/configureClubs/page.js
@@ -4,6 +4,7 @@ import {useState, useEffect} from 'react'
 const ConfigureClubs = () => {
 
     const [clubs, setClubs] = useState([]);
+    const [filter, setFilter] = useState(0)
     const [filteredClub, setFilteredClub] = useState("Frontend");
 
     useEffect(() => {
@@ -21,22 +22,35 @@ const ConfigureClubs = () => {
 
     return (
         <div>
+
             <nav className="w-[90vw] mx-auto mt-4 h-16 rounded-xl flex flex-row  bg-gradient-to-r from-purple-600/60 to-blue-500/60 items-center justify-evenly">
-                <h1 className="text-lg hover:bg-white cursor-pointer hover:text-sky-400 rounded-xl py-2 px-4 text-white font-bold">Frontend</h1>
-                <h1 className="text-lg hover:bg-white cursor-pointer hover:text-sky-400 rounded-xl py-2 px-4 text-white font-bold">Backend</h1>
-                <h1 className="text-lg hover:bg-white cursor-pointer hover:text-sky-400 rounded-xl py-2 px-4 text-white font-bold">Devops</h1>
-                <h1 className="text-lg hover:bg-white cursor-pointer hover:text-sky-400 rounded-xl py-2 px-4 text-white font-bold">UI/UX</h1>
-                <h1 className="text-lg hover:bg-white cursor-pointer hover:text-sky-400 rounded-xl py-2 px-4 text-white font-bold">CyberSecurity</h1>
-                <h1 className="text-lg hover:bg-white cursor-pointer hover:text-sky-400 rounded-xl py-2 px-4 text-white font-bold">iOS/Android</h1>
-                <h1 className="text-lg hover:bg-white cursor-pointer hover:text-sky-400 rounded-xl py-2 px-4 text-white font-bold">Blockchain</h1>
+                
+                {clubs?(
+                    
+                    clubs.map((club,idx)=>{
+                        return(
+                        <h1 key={idx} className="text-lg hover:bg-white cursor-pointer hover:text-sky-400 rounded-xl py-2 px-4 text-white font-bold">{club.devclub[0].Technology}</h1>
+                        )
+                    })
+ 
+                ):(<></>)}
+
             </nav>
 
             
 
         <div className='w-full justify-evenly '>
             {clubs?(
-                clubs.map((club)=>{
+                clubs.map((club,idx)=>{
                     console.log(club.devclub[0])
+                    return (
+                        <div key={idx}>
+                            <div>
+                                <img className='rounded-full w-48' src={club.devclub[0].President.Image}/>
+                                <img/>
+                            </div>
+                        </div>
+                    )
                 })
             
             ):(<></>)}
